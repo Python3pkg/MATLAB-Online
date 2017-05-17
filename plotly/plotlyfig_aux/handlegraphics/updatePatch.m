262119320e6123808de4a676864b116ac146a8d5
BRONSOLO <chuck.james.bronson@gmail.com>
2017-05-17 16:29:47 -0400

    triangulate patch to allow for mesh3d render

--- plotly/plotlyfig_aux/handlegraphics/updatePatch.m
+++ plotly/plotlyfig_aux/handlegraphics/updatePatch.m
@@ -70,119 +70,144 @@ obj.data{patchIndex}.yaxis = ['y' num2str(ysource)];
 
 %-patch type-%
 if any(nonzeros(patch_data.ZData))
-    obj.data{patchIndex}.type = 'scatter3d';
+    if obj.PlotOptions.TriangulatePatch
+        obj.data{patchIndex}.type = 'mesh3d';
+    else
+        obj.data{patchIndex}.type = 'scatter3d';
+    end
 else
     obj.data{patchIndex}.type = 'scatter'; 
 end
 
 %-------------------------------------------------------------------------%
 
-%-patch x-%
-xdata = patch_data.XData;
-if isvector(xdata)
-    obj.data{patchIndex}.x = [xdata' xdata(1)];
-else
-    xtemp = reshape(xdata,[],1);
-    xnew = [];
-    for n = 1:size(xdata,2)
-        xnew = [xnew ; xdata(:,n) ; xdata(1,n); NaN];
-    end
-    obj.data{patchIndex}.x = xnew;
-end
-
-%-------------------------------------------------------------------------%
-
-%-patch y-%
-ydata = patch_data.YData;
-if isvector(ydata)
-    obj.data{patchIndex}.y = [ydata' ydata(1)];
-else
-    ytemp = reshape(ydata,[],1);
-    ynew = [];
-    for n = 1:size(ydata,2)
-        ynew = [ynew ; ydata(:,n) ; ydata(1,n); NaN];
-    end
-    obj.data{patchIndex}.y = ynew;
-end
-
-%-------------------------------------------------------------------------%
-
-%-patch z-%
-if any(nonzeros(patch_data.ZData))
-    zdata = patch_data.ZData;
-    
-    if isvector(ydata)
-        obj.data{patchIndex}.z = [zdata' zdata(1)];
+if ~strcmp(obj.data{patchIndex}.type, 'mesh3d')
+    %-patch x-%
+    xdata = patch_data.XData;
+    if isvector(xdata)
+        obj.data{patchIndex}.x = [xdata' xdata(1)];
     else
-        ztemp = reshape(zdata,[],1);
-        znew = [];
-        for n = 1:size(zdata,2)
-            znew = [znew ; zdata(:,n) ; zdata(1,n); NaN];
+        xtemp = reshape(xdata,[],1);
+        xnew = [];
+        for n = 1:size(xdata,2)
+            xnew = [xnew ; xdata(:,n) ; xdata(1,n); NaN];
         end
-        obj.data{patchIndex}.z = znew;
+        obj.data{patchIndex}.x = xnew;
     end
-end
 
-%-------------------------------------------------------------------------%
+    %---------------------------------------------------------------------%
 
-%-patch name-%
-if ~isempty(patch_data.DisplayName);
-    obj.data{patchIndex}.name = patch_data.DisplayName;
+    %-patch y-%
+    ydata = patch_data.YData;
+    if isvector(ydata)
+        obj.data{patchIndex}.y = [ydata' ydata(1)];
+    else
+        ytemp = reshape(ydata,[],1);
+        ynew = [];
+        for n = 1:size(ydata,2)
+            ynew = [ynew ; ydata(:,n) ; ydata(1,n); NaN];
+        end
+        obj.data{patchIndex}.y = ynew;
+    end
+
+    %---------------------------------------------------------------------%
+
+    %-patch z-%
+    if any(nonzeros(patch_data.ZData))
+        zdata = patch_data.ZData;
+
+        if isvector(ydata)
+            obj.data{patchIndex}.z = [zdata' zdata(1)];
+        else
+            ztemp = reshape(zdata,[],1);
+            znew = [];
+            for n = 1:size(zdata,2)
+                znew = [znew ; zdata(:,n) ; zdata(1,n); NaN];
+            end
+            obj.data{patchIndex}.z = znew;
+        end
+    end
+
+    %---------------------------------------------------------------------%
+
+    %-patch name-%
+    if ~isempty(patch_data.DisplayName);
+        obj.data{patchIndex}.name = patch_data.DisplayName;
+    else
+        obj.data{patchIndex}.name = patch_data.DisplayName;
+    end
+
+    %---------------------------------------------------------------------%
+
+    %-patch visible-%
+    obj.data{patchIndex}.visible = strcmp(patch_data.Visible,'on');
+
+    %---------------------------------------------------------------------%
+
+    %-patch fill-%
+    obj.data{patchIndex}.fill = 'tozeroy';
+
+    %-PATCH MODE-%
+    if ~strcmpi('none', patch_data.Marker) && ~strcmpi('none', patch_data.LineStyle)
+        mode = 'lines+markers';
+    elseif ~strcmpi('none', patch_data.Marker)
+        mode = 'markers';
+    elseif ~strcmpi('none', patch_data.LineStyle)
+        mode = 'lines';
+    else
+        mode = 'none';
+    end
+
+    obj.data{patchIndex}.mode = mode;
+
+    %---------------------------------------------------------------------%
+
+    %-patch marker-%
+    obj.data{patchIndex}.marker = extractPatchMarker(patch_data);
+
+    %---------------------------------------------------------------------%
+
+    %-patch line-%
+    obj.data{patchIndex}.line = extractPatchLine(patch_data);
+
+    %---------------------------------------------------------------------%
+
+    %-patch fillcolor-%
+    fill = extractPatchFace(patch_data);
+
+    if strcmp(obj.data{patchIndex}.type,'scatter');
+        obj.data{patchIndex}.fillcolor = fill.color; 
+    else
+        obj.data{patchIndex}.surfacecolor = fill.color;
+    end
+
+    %---------------------------------------------------------------------%
+
+    %-surfaceaxis-%
+    if strcmp(obj.data{patchIndex}.type,'scatter3d');
+        minstd = min([std(patch_data.XData) std(patch_data.YData) std(patch_data.ZData)]);
+        ind = find([std(patch_data.XData) std(patch_data.YData) std(patch_data.ZData)] == minstd)-1;
+        obj.data{patchIndex}.surfaceaxis = ind; 
+    end
 else
-    obj.data{patchIndex}.name = patch_data.DisplayName;
-end
+    
+    % handle vertices
+    x_data = patch_data(1).Vertices(:,1);
+    y_data = patch_data(1).Vertices(:,2);
+    z_data = patch_data(1).Vertices(:,3);
 
-%-------------------------------------------------------------------------%
+    % specify how vertices connect to form the faces
+    i_data = patch_data(1).Faces(:,1)-1; 
+    j_data = patch_data(1).Faces(:,2)-1;
+    k_data = patch_data(1).Faces(:,3)-1;
+    
+    obj.data{patchIndex}.x = x_data; 
+    obj.data{patchIndex}.y = y_data; 
+    obj.data{patchIndex}.z = z_data; 
+    obj.data{patchIndex}.i = i_data; 
+    obj.data{patchIndex}.j = j_data; 
+    obj.data{patchIndex}.k = k_data;
 
-%-patch visible-%
-obj.data{patchIndex}.visible = strcmp(patch_data.Visible,'on');
-
-%-------------------------------------------------------------------------%
-
-%-patch fill-%
-obj.data{patchIndex}.fill = 'tozeroy';
-
-%-PATCH MODE-%
-if ~strcmpi('none', patch_data.Marker) && ~strcmpi('none', patch_data.LineStyle)
-    mode = 'lines+markers';
-elseif ~strcmpi('none', patch_data.Marker)
-    mode = 'markers';
-elseif ~strcmpi('none', patch_data.LineStyle)
-    mode = 'lines';
-else
-    mode = 'none';
-end
-
-obj.data{patchIndex}.mode = mode;
-
-%-------------------------------------------------------------------------%
-
-%-patch marker-%
-obj.data{patchIndex}.marker = extractPatchMarker(patch_data);
-
-%-------------------------------------------------------------------------%
-
-%-patch line-%
-obj.data{patchIndex}.line = extractPatchLine(patch_data);
-
-%-------------------------------------------------------------------------%
-
-%-patch fillcolor-%
-fill = extractPatchFace(patch_data);
-
-if strcmp(obj.data{patchIndex}.type,'scatter');
-    obj.data{patchIndex}.fillcolor = fill.color; 
-else
-    obj.data{patchIndex}.surfacecolor = fill.color;
-end
-
-%-------------------------------------------------------------------------%
-
-%-surfaceaxis-%
-if strcmp(obj.data{patchIndex}.type,'scatter3d');
-    minstd = min([std(patch_data.XData) std(patch_data.YData) std(patch_data.ZData)]);
-    ind = find([std(patch_data.XData) std(patch_data.YData) std(patch_data.ZData)] == minstd)-1;
-    obj.data{patchIndex}.surfaceaxis = ind; 
 end
 
 %-------------------------------------------------------------------------%
